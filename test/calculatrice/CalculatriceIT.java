6d7919ffee6125edf78ac647c6e298fe9aa592b6
Eberst Emmanuel <xafree@pop-os.localdomain>
2021-12-01 15:17:08 +0100

    first

---
+++ test/calculatrice/CalculatriceIT.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatrice;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author xafree
+ */
+public class CalculatriceIT {
+       /**
+     * Test of somme method, of class Calculatrice.
+     */
+    @Test
+    public void testSomme() {
+        System.out.println("somme");
+        int a = 2;
+        int b = 3;
+        Calculatrice instance = new Calculatrice();
+        int expResult = 5;
+        int result = instance.plus(a, b);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of soustraction method, of class Calculatrice.
+     */
+    @Test
+    public void testSoustraction() {
+        System.out.println("soustraction");
+        int a = 10;
+        int b = 5;
+        Calculatrice instance = new Calculatrice();
+        int expResult = 5;
+        int result = instance.soustraction(a, b);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of division method, of class Calculatrice.
+     */
+    @Test
+    public void testDivision() {
+        System.out.println("division");
+        int diviseur = 2;
+        int diviser = 10;
+        Calculatrice instance = new Calculatrice();
+        int expResult = 5;
+        int result = instance.division(diviseur, diviser);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of mutiplication method, of class Calculatrice.
+     */
+    @Test
+    public void testMutiplication() {
+        System.out.println("mutiplication");
+        int a = 2;
+        int b = 5;
+        Calculatrice instance = new Calculatrice();
+        int expResult = 10;
+        int result = instance.mutiplication(a, b);
+        assertEquals(expResult, result);
+    }
+    
+}
---
+++ test/calculatrice/NewTestCalculatrice.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatrice;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+   CalculatriceIT.class,
+})
+
+public class NewTestCalculatrice {   
+} 
